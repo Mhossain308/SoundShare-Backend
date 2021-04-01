89f9afa83cb4ea5f7ed13006461c18fda23627f6
Yu Shi <yushiys95@gmail.com>
2021-04-01 02:49:52 -0400

    add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+# HELLO WORLD
 
 coolkid = User.create(name: "C00lkid")
 songlover = User.create(name: "Songlover123")
